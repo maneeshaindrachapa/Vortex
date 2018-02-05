1cc4f9dad73d87e154a7376f59d1ac20ee8d31eb
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-02-05 16:34:20 +0530

    According to the options wanted no of options Change

--- ionic/src/pages/create-voting-ballot/create-voting-ballot.ts
+++ ionic/src/pages/create-voting-ballot/create-voting-ballot.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { AuthServiceProvider} from '../../providers/auth-service/auth-service';
 
 @IonicPage()
 @Component({
@@ -8,14 +8,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'create-voting-ballot.html',
 })
 export class CreateVotingBallotPage {
+  username:string;
   createSuccess = false;
   ballotDetails = { ballotName: '',ballotDescription:'', startDate:'',startTime:'', endDate:'',endTime:'', url: '' };
+  noOfOptions:number;
+  ballotOptions=[];
 
-  constructor(public nav: NavController, public navParams: NavParams) {
+  constructor(public nav: NavController, public navParams: NavParams, private auth:AuthServiceProvider) {
+    this.username=this.auth.getUser(); //getting username from auth service provider
+    
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CreateVotingBallotPage');
+  //adding options according to the noofoptions
+  onChange($event){
+    this.ballotOptions=[]
+    for(var index=0;index<this.noOfOptions;index++){
+      this.ballotOptions.push(index);
+    }
   }
+  
 
 }
