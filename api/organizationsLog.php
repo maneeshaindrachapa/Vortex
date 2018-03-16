dda6c03fba5b5aaf0b4744545a2474b43e4d0fd6
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-03-16 18:13:27 +0530

    employees can signup for the accepted organizations only

--- api/organizationsLog.php
+++ api/organizationsLog.php
@@ -2,7 +2,13 @@
 include "Crud.php";
 $crud = new Crud();
 
-$query = "Select * from organization";
+$query = "SELECT * from organization";
 $data = $crud->getData($query);
+$finalData=array();
 
-echo json_encode($data);
+for($i=0;$i<sizeof($data);$i++){
+    if($data[$i]['organizationAccepted']!=0){
+        array_push($finalData,$data[$i]);
+    }
+}
+echo json_encode($finalData);
