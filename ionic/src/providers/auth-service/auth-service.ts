fc0ade5dd0cfabd4212c9a93feeaff0f8d12f6f4
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-04-29 13:22:55 +0530

    hosting URL edited

--- ionic/src/providers/auth-service/auth-service.ts
+++ ionic/src/providers/auth-service/auth-service.ts
@@ -28,7 +28,7 @@ export class AuthServiceProvider {
     } else {
       var headers = new Headers();
       headers.append('Content-Type', 'application/X-www-form=urlencoded');
-      return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/login.php",JSON.stringify({ "username": credentials.username, "password": credentials.password })).map(res => res.json());
+      return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/login.php",JSON.stringify({ "username": credentials.username, "password": credentials.password })).map(res => res.json());
     }
   }
 
@@ -44,67 +44,68 @@ export class AuthServiceProvider {
 
   //register
   public register(credentials) {
-      return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/signup.php", JSON.stringify({ "username": credentials.username, "firstname": credentials.firstname, "lastname": credentials.lastname, "password": credentials.password, "email": credentials.email, "organizationID": credentials.organizationID })).map(res => res.json());
+      return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/signup.php", JSON.stringify({ "username": credentials.username, "firstname": credentials.firstname, "lastname": credentials.lastname, "password": credentials.password, "email": credentials.email, "organizationID": credentials.organizationID })).map(res => res.json());
   }
 
   //register company
   public registerCompany(credentials) {
-      return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/registerCompany.php", JSON.stringify({ "username": credentials.username, "firstname": credentials.firstname, "lastname": credentials.lastname, "password": credentials.password, "email": credentials.email, "organizationRegNo":credentials.companyRegNo,"organizationName":credentials.companyName,"organizationAddress":credentials.address  })).map(res => res.json());
+      return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/registerCompany.php", JSON.stringify({ "username": credentials.username, "firstname": credentials.firstname, "lastname": credentials.lastname, "password": credentials.password, "email": credentials.email, "organizationRegNo":credentials.companyRegNo,"organizationName":credentials.companyName,"organizationAddress":credentials.address  })).map(res => res.json());
   }
 
   //load organizations
   public loadOrganizations() {
-    return this.http.get("https://vortexmobievotingapp.000webhostapp.com//vortex/api/organizationsLog.php").map(res => res.json())
+    return this.http.get("https://vortexmobievotingapp.000webhostapp.com/vortex/api/organizationsLog.php").map(res => res.json())
   }
 
   //getting voting ballots
   public getVotingBallots() {
-    return this.http.get("https://vortexmobievotingapp.000webhostapp.com//vortex/api/getVotingBallots.php").map(res => res.json());
+    console.log(this.username);
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/getVotingBallots.php",JSON.stringify({"username":this.username})).map(res => res.json());
   }
 
   //get voting ballots for employee
   public getEmployeeVotingBallots(username) {
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/EmployeeVotings/getEmployeeVotingBallots.php", JSON.stringify({ "username": username })).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/EmployeeVotings/getEmployeeVotingBallots.php", JSON.stringify({ "username": username })).map(res => res.json());
   }
 
   //get userdetails
   public getUserDetails(username) {
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/getUserDetails.php",JSON.stringify({ "username": username })).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/getUserDetails.php",JSON.stringify({ "username": username })).map(res => res.json());
   }
 
   //edit Profile
   public editProfile(username, userDetails) {
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/editProfile.php", JSON.stringify({ "username": username, "userDetails": userDetails })).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/editProfile.php", JSON.stringify({ "username": username, "userDetails": userDetails })).map(res => res.json());
   }
 
   //change password
   public changePassword(username, passwordDetails) {
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/changePassword.php", JSON.stringify({ "username": username, "passwordDetails": passwordDetails })).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/changePassword.php", JSON.stringify({ "username": username, "passwordDetails": passwordDetails })).map(res => res.json());
   }
 
   //getRequestNo
   public getRequestNo(username) {
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/getRequestNo.php",JSON.stringify({ "username": username})).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/getRequestNo.php",JSON.stringify({ "username": username})).map(res => res.json());
   }
 
   //get unaccepted users
   public getUnacceptedUsers(username) {
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/getUnacceptedUsers.php", JSON.stringify({ "username": username})).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/getUnacceptedUsers.php", JSON.stringify({ "username": username})).map(res => res.json());
   }
 
   //accept user
   public acceptUser(username) {
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/acceptUser.php", JSON.stringify({ "username": username})).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/acceptUser.php", JSON.stringify({ "username": username})).map(res => res.json());
   }
 
   //get unaccepted organia=zations
   public getUnacceptedOrganizations(){
-    return this.http.get("https://vortexmobievotingapp.000webhostapp.com//vortex/api/getUnacceptedOrganizations.php").map(res => res.json());
+    return this.http.get("https://vortexmobievotingapp.000webhostapp.com/vortex/api/getUnacceptedOrganizations.php").map(res => res.json());
   }
 
   //accept an organization
   public acceptOrganizations(organizationID){
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/acceptOrganization.php", JSON.stringify({ "oraganizationID": organizationID})).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/acceptOrganization.php", JSON.stringify({ "oraganizationID": organizationID})).map(res => res.json());
   }
 
 }
--- ionic/src/providers/ballot-service/ballot-service.ts
+++ ionic/src/providers/ballot-service/ballot-service.ts
@@ -31,20 +31,20 @@ export class BallotServiceProvider {
 
   //get voting options
   public getvotingOptions(votingBallotID) {
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/EmployeeVotings/getVotingOptions.php", JSON.stringify({ "votingballotID": votingBallotID })).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/EmployeeVotings/getVotingOptions.php", JSON.stringify({ "votingballotID": votingBallotID })).map(res => res.json());
   }
   //get results
   public getResults(votingBallotID,votingOptionIDs){
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/EmployeeVotings/getResults.php", JSON.stringify({ "votingballotID": votingBallotID,"votingoptionIDs":votingOptionIDs })).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/EmployeeVotings/getResults.php", JSON.stringify({ "votingballotID": votingBallotID,"votingoptionIDs":votingOptionIDs })).map(res => res.json());
   }
 
   //add voting
   public employeeAddVote(vote) {
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/EmployeeVotings/AddEmployeeVote.php", JSON.stringify({ "votingballotID": this.votingBallotID, "username": this.username, "votingoptionID": vote })).map(res => res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/EmployeeVotings/AddEmployeeVote.php", JSON.stringify({ "votingballotID": this.votingBallotID, "username": this.username, "votingoptionID": vote })).map(res => res.json());
   }
   //make a voting ballot
   public createVotingBallot(ballotDetails, noOfOptions, ballotOptionsContainer) {
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/createBallot.php", JSON.stringify({
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/createBallot.php", JSON.stringify({
       "username": this.username,
       "ballotName": ballotDetails.ballotName,
       "ballotDescription": ballotDetails.ballotDescription,
@@ -57,12 +57,12 @@ export class BallotServiceProvider {
     })).map(res => res.json());
   }
   public checkSecurity(votingballotid,publicsecuritykey,privatesecuritykey,username){
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/checkSec.php",JSON.stringify({"votingballotID":votingballotid,"publicSecurityKey":publicsecuritykey,"privateSecurityKey":privatesecuritykey,"username":username})).map(res=>res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/checkSec.php",JSON.stringify({"votingballotID":votingballotid,"publicSecurityKey":publicsecuritykey,"privateSecurityKey":privatesecuritykey,"username":username})).map(res=>res.json());
   }
   public sendMail(votingballotid,username){
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/sendMail.php",JSON.stringify({"votingballotID":votingballotid,"username":username})).map(res=>res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/sendMail.php",JSON.stringify({"votingballotID":votingballotid,"username":username})).map(res=>res.json());
   }
   public removeBallot(votingballotid){
-    return this.http.post("https://vortexmobievotingapp.000webhostapp.com//vortex/api/removeBallot.php",JSON.stringify({"votingballotID":votingballotid})).map(res=>res.json());
+    return this.http.post("https://vortexmobievotingapp.000webhostapp.com/vortex/api/removeBallot.php",JSON.stringify({"votingballotID":votingballotid})).map(res=>res.json());
   }
 }
