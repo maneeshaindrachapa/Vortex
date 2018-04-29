853a0d324e548336685ef8871bb1dab4eb7a653d
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-04-29 13:20:53 +0530

    manager page voting ballots loading according to the conpany

--- api/getVotingBallots.php
+++ api/getVotingBallots.php
@@ -2,7 +2,23 @@
 include "Crud.php";
 $crud = new Crud();
 
-$query = "select * from votingballot order by votingballotID desc";
-$data = $crud->getData($query);
+$data = json_decode(file_get_contents("php://input"));
 
-echo json_encode($data);
+if(sizeof($data)!=0){
+    $username=$data->username;
+    $query1="start transaction";
+    $data1=$crud->execute($query1);
+
+    $query2="select organizationID from user where username='$username'";
+    $data2=$crud->getData($query2);
+    $organizationID=$data2[0]['organizationID'];
+
+    $query3 = "select * from votingballot where organizationID='$organizationID' order by votingballotID desc";
+    $data3 = $crud->getData($query3);
+
+    $query4="commit";
+    $data4=$crud->execute($query4);
+    //if($data4){
+        echo json_encode($data3);
+    //}
+}
