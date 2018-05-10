731f53d0b39edd0efe31fd3553a6b8421d04ea27
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-05-10 12:03:35 +0530

    add a mock to alert controller

--- ionic/src/mocks.ts
+++ ionic/src/mocks.ts
@@ -61,3 +61,16 @@ export class ConfigMock {
       });
     }
   }
+  
+  export class LoadingControllerMock {
+      _getPortal(): any { return {} };
+      create(options?: any) { 
+          return new LoadingMock()
+      };
+  }
+
+  class LoadingMock {
+      present() { };
+      dismiss() { };
+      dismissAll() { };
+  }
