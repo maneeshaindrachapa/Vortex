397e28d00315f0a337396487cf75da7264f0489e
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-05-13 00:27:09 +0530

    update mocks

--- ionic/src/mocks.ts
+++ ionic/src/mocks.ts
@@ -63,6 +63,11 @@ export class ConfigMock {
         resolve();
       });
     }
+    public open(): any {
+      return new Promise((resolve: Function) => {
+        resolve();
+      });
+    }
   }
   
   export class LoadingControllerMock {
@@ -77,7 +82,7 @@ export class ConfigMock {
       dismiss() { };
       dismissAll() { };
   }
-
+  
   export class AuthServiceMock {
     //mock data
     organizationsUA='[{"organizationID":"13","organizationRegNo":"1231235","organizationName":"company1","organizationAddress":"abc,abc,abc","organizationAccepted":"0"},{"organizationID":"14","organizationRegNo":"1231236","organizationName":"company3","organizationAddress":"lane1,lane2,srilanka","organizationAccepted":"0"}]';
@@ -85,12 +90,17 @@ export class ConfigMock {
     username="maneesha";
     userDetails={"username":'maneesha','password':'test'};
     userDetails_={"username":"maneesha","password":"f5bb0c8de146c67b44babbf4e6584cc0","type":"2","accepted":"1"}
+    userDetails__={"username":"maneesha","firstname":"maneesha","lastname":"indrachapa","email":"maneeshaindrachapa@gmail.com"}
     registerDetails={ "username": 'maneesha', "firstname": 'maneesha', "lastname": 'indrachapa', "password": '123123123', "email": 'maneeshaindrachapa@gmail.com', "organizationID": '1' };
     loadorganizations='[{"organizationID":"1","organizationRegNo":"01072921","organizationName":"Pizza Hut","organizationAddress":"No:77\/43,Isuru Pedesa,Mampitiya,Galle","organizationAccepted":"1"}]';
     regCodes_='4';
     unacceptedUsers='[{"id":"22","username":"employee1","firstname":"employeeFN","lastname":"employeeLN","password":"f5bb0c8de146c67b44babbf4e6584cc0","email":"employee1@gmail.com","type":"1","organizationID":"1","accepted":"0"}]';
     username_m='maneesha';
     organizationCred={ "username": 'manager', "firstname": 'managerFN', "lastname": 'managerLN', "password": '123123123', "email": 'manager@gmail.com', "organizationRegNo":'12312312',"organizationName":'organization1',"organizationAddress":'organization1,sri lanka'  }
+    passwordChangeCode='1';
+    passwordDetails={"currentPassword":'123123123',"newPassword":'123123123'};
+    getEmployeeVotingBallots__=[{"votingballotID":"30","organizationID":"1","votingballotName":"Test 0","votingballotDescription":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed augue sed enim venenatis egestas. Nulla sit amet lacus eget velit consequat convallis ac eget turpis. Morbi sed bibendum nulla, id vehicula ipsum.","startDate":"2018-03-01","startTime":"15:53:30","endDate":"2018-03-08","endTime":"07:53:30","image":"https:\/\/encrypted-tbn0.gstatic.com\/images?q=tbn:ANd9GcRJaDcCDuvFPckG9uPaFCkolwsRWZI6Av1DdwR0wQY_PpGEaVT_3A","noOfOptions":"3","publicKey":"902245","voterID":"45","username":"nipuna","votingoptionID":"35","privateKey":"7634fa43bf6b2a16d7508c17a7c7a9e0"}];
+    votingballots__=[{"votingballotID":"36","organizationID":"1","votingballotName":"Test 1","votingballotDescription":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed augue sed enim venenatis egestas. Nulla sit amet lacus eget velit consequat convallis ac eget turpis. Morbi sed bibendum nulla, id vehicula ipsum.","startDate":"2018-05-10","startTime":"06:29:15","endDate":"2018-05-13","endTime":"06:29:15","image":"https:\/\/static.angloinfo.com\/images\/assets\/original\/d7fedd2c\/fc8c\/d7fedd2c-fc8c-4caf-93c0-ab0faf87112c.jpg","noOfOptions":"2","publicKey":"511916"},{"votingballotID":"30","organizationID":"1","votingballotName":"Test 0","votingballotDescription":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed augue sed enim venenatis egestas. Nulla sit amet lacus eget velit consequat convallis ac eget turpis. Morbi sed bibendum nulla, id vehicula ipsum.","startDate":"2018-03-01","startTime":"15:53:30","endDate":"2018-03-08","endTime":"07:53:30","image":"https:\/\/encrypted-tbn0.gstatic.com\/images?q=tbn:ANd9GcRJaDcCDuvFPckG9uPaFCkolwsRWZI6Av1DdwR0wQY_PpGEaVT_3A","noOfOptions":"3","publicKey":"902245"}];
 
     //methods
     public register(registerDetails):Observable<any> {
@@ -121,6 +131,18 @@ export class ConfigMock {
     }
     public registerCompany(organizationCred):Observable<any>{
       return Observable.of(this.regCodes_);
+    }
+    public getUserDetails(username):Observable<any>{
+      return Observable.of(this.userDetails__);
+    }
+    public changePassword(username, passwordDetails):Observable<any>{
+      return Observable.of(this.passwordChangeCode);
+    }
+    public getEmployeeVotingBallots(username):Observable<any>{
+      return Observable.of(this.getEmployeeVotingBallots__);
+    }
+    public getVotingBallots():Observable<any>{
+      return Observable.of(this.votingballots__);
     }
   }
 
